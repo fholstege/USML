8983929c29aa1593d24d0ddad2bb56d7b36ca059
fholstege <31654652+fholstege@users.noreply.github.com>
2021-01-15 21:51:31 +0100

    Working implementation of PCA, first shot at sparse PCA

---
+++ Assignment1/Analysis.R
@@ -0,0 +1,40 @@
+
+# Packes required for subsequent analysis. P_load ensures these will be installed and loaded. 
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(PMA, 
+               ggplot2)
+
+# load the data
+load("../Data/cityweather.RData")
+
+# load helper functions
+source("../Helpers/utilities.R")
+source("PCA.R")
+
+# set seed to ensure reproducability
+set.seed(123)
+
+
+### Compare our PCA function to the prcomp() function in R
+# We get the exact same results as prcomp 
+result_prcomp <- prcomp(scale(as.matrix(cityweather)))
+result_ours <- svd_components(scale(as.matrix(cityweather)))
+
+### create an scree plot
+screePlot <- create_screePlot(as.matrix(scale(cityweather)))
+screePlot
+
+
+
+### Compare our sparse PCA function to the PMD() function in R
+result_ours <- sparce_PCA(as.matrix(cityweather), c1=1, c2=1)
+result_ours$v
+result_ours$u
+result_ours$sigma
+
+result_pmd <- PMD(as.matrix(cityweather), sumabsu = 1, sumabsv = 1, center = FALSE)
+result_pmd$v
+result_pmd$u
+result_pmd$d
+
+# the results appear quite similar, but not exactly - not sure if due to convergence, our initial u
---
+++ Assignment1/PCA.R
@@ -0,0 +1,113 @@
+
+
+####
+# svd_components: extracts the principal components and their explained variaance from a matrix X
+# arguments: 
+#     mX: a matrix object
+# output: 
+#     result: a list object with the components and their explained variance
+svd_components <- function(mX){
+  
+  # parameters; k columns, n observations, vector i of ones
+  k = ncol(mX)
+  n <- dim(mX)[1]
+  i <- rep(1,n)
+  
+  # use this matrix H to center matrix X
+  mH <- (diag(n) -  (1/n) * i %*% t(i))
+  mX_center <-mH %*% mX
+  
+  # get use single value decomposition (svd)
+  lSvd_mX <- svd(mX_center)
+  
+  # extract Principal components and explained variance from single value decomposition
+  mPComponents <- lSvd_mX$v[,1:k]
+  vExplained_var <- 1/(n-1)*(lSvd_mX$d^2)[1:k]
+  
+  # create list of results, with components and explained var for each
+  result <- list(components=mPComponents,     
+                 explained_variance=vExplained_var,
+                 d = lSvd_mX$d)
+  
+  return(result)
+}
+
+
+####
+# sparce_PCA: applies sparce PCA to a matrix X
+# arguments: 
+#     mX: a matrix object
+#     c1: a constant, influences penalty term by constraining L1 norm of vector v 
+#     c2: a constant, influences penalty term by constraining L1 norm of vector u
+# output: 
+#     result: a list object with the vector v, u, and sigma 
+sparce_PCA <- function(mX, c1, c2, iMax=100){
+  
+  # create initial v
+  v <-  as.matrix(svd_components(mX)$components[,1])
+  
+  # create initial u
+  u <- mX %*% v
+  
+  # start while loop
+  i = 0
+  while(i < iMax){
+    
+    # update each iteration
+    i <- i + 1
+
+    # define matrix Xv
+    mXv <- mX %*% v
+
+    # find lambda 1 with binary search
+    lambda1 <- binary_search(u, c1)
+    
+    ## Floris: I am not sure when the while loop should break, e.g. when the algorithm has converged
+    if(lambda1 == 0){
+      break
+    }
+    
+    # get u, given V
+    u  <- soft_l2norm(mXv, lambda1)
+
+    # define matrix Xu
+    mXu <- t(mX) %*% u
+
+    # then, determine lambda 2
+    lambda2 <- binary_search(v, c2)
+
+    # redefine v
+    v <- soft_l2norm(mXu, lambda2)
+    
+  }
+  
+  # calculate sigma
+  sigma = t(u) %*% mX %*% v
+  
+  # return the results
+  results <- list(v = v, u = u, sigma = sigma)
+  return(results)
+}
+
+
+create_screePlot <- function(mX){
+  
+  result_PCA <- svd_components(mX)
+  
+  screePlot <- ggplot(data=data.frame(result_PCA), aes(x=seq(1,length(explained_variance),1), y=explained_variance))+
+                      geom_line(linetype="dashed",color="red", size=2)+
+                      geom_point(color="blue", size=4)+
+                      labs(x = "Component", y="Explained variance")+
+                      theme_minimal() +
+                      theme(axis.text.x = element_text(size = 14), axis.title.x = element_text(size = 16),
+                            axis.text.y = element_text(size = 10), axis.title.y = element_text(size = 16))  
+  return(screePlot)
+  
+  
+}
+
+
+
+
+
+
